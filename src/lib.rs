19a9745fbefcf073d642250076ea47e9626aa380
Alexander Lebedev <hardskulls@rambler.ru>
2024-02-06 23:17:36 +0300

    format: Formatted code.

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use std::time::{Duration, Instant};
 
 /// Measures the execution time of a function once and returns the duration.
 #[inline]
-pub fn bench_once<F, T>(f : F) -> Duration
-    where
-        F : FnOnce() -> T,
+pub fn bench_once<F, T>(f: F) -> Duration
+where
+    F: FnOnce() -> T,
 {
     let instant = Instant::now();
     f();
@@ -16,14 +16,13 @@ pub fn bench_once<F, T>(f : F) -> Duration
 /// 'iterations' defines how many measurements there will be.
 /// Returns the lowest value, thereby minimizing OS influence on results.
 #[inline]
-pub fn bench_times<F, T>(iterations : u32, mut f : F) -> Option<Duration>
-    where
-        F : FnMut() -> T,
+pub fn bench_times<F, T>(iterations: u32, mut f: F) -> Option<Duration>
+where
+    F: FnMut() -> T,
 {
     let cap = iterations.try_into().unwrap_or(0);
     let mut vec = Vec::with_capacity(cap);
-    for _ in 0..iterations
-    {
+    for _ in 0..iterations {
         let elapsed_time = bench_once(&mut f);
         vec.push(elapsed_time);
     }
@@ -32,11 +31,11 @@ pub fn bench_times<F, T>(iterations : u32, mut f : F) -> Option<Duration>
 
 /// Calculates the number of iterations of `bench_once` that can be executed within a specified time limit.
 #[inline]
-pub fn calc_iterations(one_measurement_takes : Duration, desired_time : Duration) -> u32
-{
+pub fn calc_iterations(one_measurement_takes: Duration, desired_time: Duration) -> u32 {
     let mut div = 1;
-    while desired_time / div > one_measurement_takes
-    { div *= 10 }
+    while desired_time / div > one_measurement_takes {
+        div *= 10
+    }
     // For some reason, testing with an unmodified answer takes twice the expected time;
     // that's why there is a correction here.
     div / 2
