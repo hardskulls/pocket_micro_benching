68524e340c2f11cedf478c2002b2f0223d956995
Alexander Lebedev <hardskulls@rambler.ru>
2024-02-21 18:07:14 +0300

    refactor: Improved code simplicity and correctness.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::time::{Duration, Instant};
 
+pub type Iterations = u128;
+
 /// Measures the execution time of a function once and returns the duration.
 #[inline]
 pub fn bench_once<F, T>(f: F) -> Duration
@@ -16,27 +18,15 @@ where
 /// 'iterations' defines how many measurements there will be.
 /// Returns the lowest value, thereby minimizing OS influence on results.
 #[inline]
-pub fn bench_times<F, T>(iterations: u32, mut f: F) -> Option<Duration>
+pub fn bench_times<F, T>(iterations: Iterations, mut f: F) -> Option<Duration>
 where
     F: FnMut() -> T,
 {
-    let cap = iterations.try_into().unwrap_or(0);
-    let mut vec = Vec::with_capacity(cap);
-    for _ in 0..iterations {
-        let elapsed_time = bench_once(&mut f);
-        vec.push(elapsed_time);
-    }
-    vec.into_iter().min()
+    (1..iterations).map(|_| bench_once(&mut f)).min()
 }
 
 /// Calculates the number of iterations of `bench_once` that can be executed within a specified time limit.
 #[inline]
-pub fn calc_iterations(one_measurement_takes: Duration, desired_time: Duration) -> u32 {
-    let mut div = 1;
-    while desired_time / div > one_measurement_takes {
-        div *= 10
-    }
-    // For some reason, testing with an unmodified answer takes twice the expected time;
-    // that's why there is a correction here.
-    div / 2
+pub fn calc_iterations(single_run: Duration, total: Duration) -> Iterations {
+    total.as_nanos() / single_run.as_nanos()
 }
